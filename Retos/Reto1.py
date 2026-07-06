349bea2f8698e7ffb93e7e047ae2436a35d1217e
Ignacio Sáez <is2542000@gmail.com>
2026-07-06 10:54:57 -0400

    Retos

--- Retos/Reto1.py
+++ Retos/Reto1.py
@@ -1,25 +1,19 @@
-"""En la asignatura de Programación de la carrera de Ingeniería Civil en Informática,
-un estudiante ha rendido sus primeras 3 calificaciones de tareas prácticas de
-laboratorio. La aprobación de la asignatura exige calcular una nota final basada
-en los siguientes pesos o ponderaciones para cada laboratorio:
-Laboratorio 1: 40% de la nota finalssf
-Laboratorio 2: 40% de la nota final
-Laboratorio 3: 20% de la nota final"""
-nota1 = float(input(f"Ingrese la nota del laboratorio 1: "))
-nota2 = float(input(f"Ingrese la nota del laboratorio 2: "))
-nota3 = float(input(f"Ingrese la nota del laboratorio 3: "))
+print("Ingresa tus notas")
+nota1 = float(input("Ingresa la nota del Laboratorio 1 : "))
+nota2 = float(input("Ingresa la nota del Laboratorio 2 : "))
+nota3 = float(input("Ingresa la nota del Laboratorio 3 : "))
 
-notas = [nota1], [nota2], [nota3]
-print(notas)
-
-porcentaje1 = notas[0] * 40
-porcentaje2 = notas[1] * 20
-porcentaje3 = notas[2] * 20
+lista_notas = [nota1, nota2, nota3]
 
 
+promedio_final = (lista_notas[0] * 0.40) + (lista_notas[1] * 0.40) + (lista_notas[2] * 0.20)
 
-print(f"Nota lab 1 [nota1]")
 
+print(" Reporte de Notas:")
+print(f"Laboratorio 1 (40%): {lista_notas[0]}")
+print(f"Laboratorio 2 (40%): {lista_notas[1]}")
+print(f"Laboratorio 3 (20%): {lista_notas[2]}")
 
+print(f"Promedio: {round(promedio_final, 2)}")
 
 
