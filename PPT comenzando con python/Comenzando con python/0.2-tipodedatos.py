f2901baa82cf362b604a73b9056b759051901f93
Ignacio Sáez <is2542000@gmail.com>
2026-07-06 10:58:17 -0400

    PPT comenzando con python

---
+++ PPT comenzando con python/Comenzando con python/0.2-tipodedatos.py
@@ -0,0 +1,73 @@
+# Datos Numericos
+PI = 3.1415926535 
+edad = 19
+annio_nacimiento = 2007
+
+# Numeros flotantes
+Estatura = 1.75
+Peso = 75
+
+# Operacion aritmetica basica (area de un triangulo)
+base = 8.7
+altura = 12.5
+area = (base * altura) / 2
+
+# Salida de numeros
+print(f"El numero PI con 1 decimales es: {PI:.1f}")
+
+# numeros complejos
+num_complejo = 4 + 2j 
+otro_complejo = complex(4.2) 
+
+# Redondear
+area_redondeada = round(area)
+print(f"El area del triangulo redondeada es: {area_redondeada} cm")
+
+# transformaciones
+print(float(edad))
+
+# Cadenas de texto
+carrera = "Ingenieria Civil en Informatica"
+institucion = "Universidad los Lagos"
+
+# Imprimir la posicion del caracter
+print(carrera[0])
+print(carrera[30])
+
+print("Hola" * 4) 
+
+print(carrera[0:11]) 
+
+# Metodo len permite contar caracteres
+print(len(institucion))
+
+# ARREGLOS (LISTAS)
+print("Arreglos de texto")
+colores = ["Azul", "Rojo", "Verde", "Amarillo"]
+numeros = [1, 2, 3, 4, 5, 6]
+
+print(colores)
+print(numeros)
+
+# Boleanos (Logicos)
+# Recuerda que los nombres de variables no llevan espacios (bien hecho con el guion bajo)
+interruptor = False
+Luz_electrica = True 
+
+print("Boleanos")
+print(interruptor)
+print(Luz_electrica)
+
+# Type
+print(f"El tipo de datos es {type(num_complejo).__name__}")
+
+# Evaluando datos bool
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool("false"))
+
+# Evaluando numeros con operadores de comparacion
+print(100 <= 50)
+print(10 == 10)
+print(20 < 0)
---
+++ PPT comenzando con python/Comenzando con python/04.tuplas.py
@@ -0,0 +1,23 @@
+# TUPLAS 
+
+estudiantes = ("Matias", "Franscisco", "Alan", "Maykol")
+print(type(estudiantes))
+
+# Creando una tupla compleja con datos estructurados
+datos = ([1,2,3,4,5], ("Queilen", "Castro"), ("Universidad de Los Lagos", "AIEP"))
+
+# Tambien se puede consultar la posicion de un elemento al igual que la lista
+print(datos[0])
+print(datos)
+
+# Con las listas se pueden eliminar elementos
+lista_asignaturas =['Programacion', 'Quimica', 'Introduccion ']
+print(f"LISTA: {lista_asignaturas}")
+
+#No se puede eliminar con pop una tupla porque es inmutable
+#lista_asignaturas.pop()
+#print(f"LISTA CON ELEMENTO ELIMINADO: {lista_asignaturas}")""""
+
+print(estudiantes.index("Alan"))#se encuentra en la posicion 2
+
+print(sorted(estudiantes))
