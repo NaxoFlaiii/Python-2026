328a7a2b0e12663a20058f155830d29d037a8dbe
Ignacio Sáez <is2542000@gmail.com>
2026-07-12 21:20:54 -0400

    Comenzando con python

--- PPT comenzando con python/Comenzando con python/12.funciones.py
+++ PPT comenzando con python/Comenzando con python/12.funciones.py
@@ -24,12 +24,49 @@ print("Resultado 1 (b por defecto):", resultado1)
 resultado2 = resta(4,4)
 print("Resultado 2 (b especificado):", resultado2)
 
+def calcular_potencia(base, exponente):
+    return base ** exponente
+
+# Llamada con argumentos por nombre (el orden no importa)
+resultado = calcular_potencia(exponente=3, base=2)
+print(resultado)
+
 def factorial_normal(n):
     r = 1
     i = 2
     while i <= n:
         r *= i
-        i += 1  
-    return r    
+        i += 1
+    return r
 
-print(factorial_normal(5))  
+print(factorial_normal(5))
+
+def factorial_recursivo(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial_recursivo(n - 1)
+
+print(factorial_recursivo(5))
+
+def fibonacci_recursivo(n):
+    # Casos base (0 y 1)
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    # Caso recursivo
+    return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2)
+
+# Ejemplo: 5to número de Fibonacci (Se empieza del cero)
+print(f"5to Número de Fibonacci: {fibonacci_recursivo(5)}")
+
+def fibonacci_iterativo(n):
+    # Casos base (0 y 1)
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b  # Se actualizan los valores
+    return a
+
+# Ejemplo: 5to número de Fibonacci (Se empieza del cero)
+print(f"5to Número de Fibonacci: {fibonacci_iterativo(5)}")
